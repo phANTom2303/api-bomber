3d5d778342dc5e0284fb2778018623f7d0cfa868
phaANTom2303 <anikavy.goenka@gmail.com>
2026-04-09 00:37:28 +0530

    simple api hammering

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+#include <curl/curl.h>
+
 #include <algorithm>
 #include <chrono>
 #include <iostream>
-#include <string>
 #include <json.hpp>
-#include <curl/curl.h>
+#include <string>
 
 using namespace std;
 
@@ -12,27 +13,26 @@ using namespace std;
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     // Calculate the total size of the incoming chunk
     size_t totalSize = size * nmemb;
-    
+
     // Cast the user pointer back to a std::string and append the data
     ((std::string*)userp)->append((char*)contents, totalSize);
-    
+
     // Return the number of bytes processed so libcurl knows we handled it
     return totalSize;
 }
 
-void makeApiCall(string & URL){
+void makeApiCall(string& URL) {
     CURL* curl;
     CURLcode res;
-    string readBuffer; // This will hold the raw data
-
-    // Initialize libcurl globally
-    curl_global_init(CURL_GLOBAL_DEFAULT);
+    string readBuffer;  // This will hold the raw data
 
     // Initialize an easy session
     curl = curl_easy_init();
-    
-    if(curl) {
+
+    if (curl) {
         // 1. Set the URL you want to GET
+        // Note : URL.c_str() is converting a cpp string object to c style
+        // string i.e. char *
         curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
 
         // 2. Pass our custom callback function to handle incoming data
@@ -45,8 +45,9 @@ void makeApiCall(string & URL){
         res = curl_easy_perform(curl);
 
         // Check for errors
-        if(res != CURLE_OK) {
-            cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+        if (res != CURLE_OK) {
+            cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res)
+                 << endl;
         } else {
             // Print the raw data received
             cout << "--- Raw Data Received ---\n";
@@ -57,25 +58,42 @@ void makeApiCall(string & URL){
         // Always cleanup when done
         curl_easy_cleanup(curl);
     }
+}
+
+vector<double> hammerURL(string& URL) {
+    vector<double> responseTimes;
+
+    for (int i = 0; i < 10; i++) {
+        auto start = chrono::high_resolution_clock::now();
+
+        makeApiCall(URL);
+
+        auto stop = chrono::high_resolution_clock::now();
+
+        auto duration_obj =
+            chrono::duration_cast<chrono::microseconds>(stop - start);
+
+        auto actualDuration = duration_obj.count();
+
+        double durationInMS = (actualDuration / 1000.0);
+
+        responseTimes.push_back(durationInMS);
+    }
+
+    return responseTimes;
+}
+int main() {
+    // Initialize libcurl globally
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    string URL = "https://jsonplaceholder.typicode.com/todos/1";
+
+    vector<double> responseTimes = hammerURL(URL);
+    for (double& durationInMS : responseTimes)
+        cout << "Duration = " << durationInMS << " ms" << endl;
 
     // Global cleanup
     curl_global_cleanup();
-}
-int main() {
-    string URL = "https://jsonplaceholder.typicode.com/todos/";
-   
-
-    auto start = chrono::high_resolution_clock::now();
-
-    makeApiCall(URL);
-
-    auto stop = chrono::high_resolution_clock::now();
-
-    auto duration_obj = chrono::duration_cast<chrono::microseconds>(stop - start);
-
-    auto actualDuration = duration_obj.count();
-    
-    cout << "Duration = " << actualDuration << " microseconds" << endl;
 
     return 0;
 }
